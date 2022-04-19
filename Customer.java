9c403d07c109cf01e5836ce6734739475d782ba8
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-19 13:39:29 +0100

    Customer information input and build gradle

---
+++ Customer.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
+public class Customer {
+    String customerName;
+    String customerSurname;
+    String customerEmail;
+
+
+    public Customer(String name, String surname, String email) {
+        customerName = name;
+        customerSurname = surname;
+        customerEmail = email;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first name");
+
+        String name = input.nextLine();
+
+        System.out.println("Enter surname");
+
+        String surname = input.nextLine();
+
+        System.out.println(name);
+        System.out.println(surname);
+
+        Customer customer1 = new Customer("Rosa", "Salih", "hi@sky.uk");
+        System.out.println(customer1.customerName + customer1.customerSurname + customer1.customerEmail);
+
+    }
+}
