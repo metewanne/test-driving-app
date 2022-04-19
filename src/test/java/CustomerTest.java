73fc5edef6263dff77abaa585895b1ab81989eff
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-19 14:06:57 +0100

    Restructure directory and created test class

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CustomerTest {
+    @Test
+    public void noInput() throws InvalidInputException {
+
+    }
+}
