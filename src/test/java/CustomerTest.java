a64ff399b8fab6757ae3d77135423e00f9e19cf5
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-21 15:23:52 +0100

    Format change to Customer.java and removed unused dependencies in CustomerTest

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,8 +1,3 @@
-
-import org.assertj.core.api.MapAssert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
