81292dc93127fcf63bf0eafabe98736f7c0b825c
rosa03 <rsalih03@hotmail.com>
2022-04-21 15:28:25 +0100

    cleaned up imports

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,13 +1,6 @@
-
-import org.assertj.core.api.MapAssert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.*;
 
 public class CustomerTest {
 
