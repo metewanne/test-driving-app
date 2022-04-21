0d33cd7128c323b26917250056556c034387955a
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-21 15:17:27 +0100

    Amended dependencies, changed customer input and added test cases

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,27 +1,11 @@
-package main.java;
-
-import java.util.Scanner;
-
 public class Customer {
-    String customerName;
-    String customerSurname;
-    String customerEmail;
 
-
-    public Customer(String name, String surname, String email) {
-        customerName = name;
-        customerSurname = surname;
-        customerEmail = email;
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter first name");
-        String name = input.nextLine();
-        System.out.println("Enter surname");
-        String surname = input.nextLine();
-        System.out.println("Enter email");
-        String email = input.nextLine();
+    public String inputCustomerName(String customerName) throws Exception {
+        if (customerName == null || customerName.trim().equals("")) {
+            throw new Exception("Incorrect input");
+        }
+        return "Hi";
 
     }
+
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,10 +1,34 @@
-import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import org.assertj.core.api.MapAssert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.*;
 
 public class CustomerTest {
+
+    Customer customer1 = new Customer();
+
     @Test
-    public void noInput() throws InvalidInputException {
+    public void testEmptyInput() throws Exception {
+
+        assertThrows(Exception.class, () -> customer1.inputCustomerName(""));
 
     }
+    @Test
+    public void testStuff() throws Exception {
+
+        String name = customer1.inputCustomerName("Hi");
+
+        assertThat(name).isEqualTo("Hi");
+
+
+    }
+
+
+
 }
