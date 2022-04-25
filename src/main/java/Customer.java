0933f9f78e043a631635ec3d1c59bd33c59adf67
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-25 14:52:22 +0100

    Created scanner input, new test for email input and fixed error bugs.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,24 +1,43 @@
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Customer {
 
-    public String inputCustomerName(String customerName) throws Exception {
 
-        if (customerName == null || customerName.trim().equals("")) {
+    public static Scanner inputCustomerName(Scanner scanName) throws Exception {
+
+        String name = scanName.nextLine();
+
+        if (name == null || name.trim().equals("")) {
             throw new Exception("Empty input");
         }
-
-        char[] letters = customerName.toCharArray();
+        char[] letters = name.toCharArray();
         for (char c : letters) {
             if (Character.isDigit(c)) {
                 throw new Exception("Incorrect type of input");
             }
         }
 
-        return "Hi";
+        return scanName;
+    }
 
+    public static boolean patternMatches(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanName = new Scanner(System.in);
+        System.out.print("Enter first name");
+        String firstName = scanName.nextLine();
+
+        System.out.print("Enter surname");
+        String surname = scanName.next();
+
+
+        inputCustomerName(scanName);
     }
 
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,16 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CustomerTest {
 
     Customer customer = new Customer();
+    Scanner scanName = new Scanner(System.in);
 
     @Test
     public void testEmptyInput() {
 
-        assertThrows(Exception.class, () -> customer.inputCustomerName(""));
+        assertThrows(Exception.class, () -> customer.inputCustomerName(scanName));
 
     }
 
@@ -18,8 +22,16 @@ public class CustomerTest {
     @Test
     public void testInputIsInt() {
 
-        assertThrows(Exception.class, () -> customer.inputCustomerName("1"));
+        assertThrows(Exception.class, () -> customer.inputCustomerName(scanName));
 
+    }
+
+    @Test
+    public void testUsingSimpleRegex() {
+        String emailAddress = "username@domain.com";
+        String regexPattern = "^(.+)@(\\S+)$";
+        Customer EmailValidation = null;
+        assertTrue(EmailValidation.patternMatches(emailAddress, regexPattern));
     }
 
 
