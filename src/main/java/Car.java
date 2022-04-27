431f178dded38bf62e31d4d14ea858c131852119
Afua <afua.danquah@sky.uk>
2022-04-27 09:25:20 +0100

    car and car test

---
+++ src/main/java/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+
+
+    public static void main(String [] args){
+
+
+
+    }
+
+
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CarTest {
+
+    //@Test
+    //Check that car class exists
+    @Test
+    public void testCheckClass(){
+        Car car = new Car();
+    }
+
+    //@Test
+    //Check the car object is not empty
+
+    //@Test
+    //Check the car object has a list of brands
+
+
+
+
+//    @Test
+//    public void testCarClass(){
+//        assertThrows(Exception.class, () -> car.);
+//    }
+
+
+}
