3ed8028296cc9eb9968cc2f9e519d7f24735172c
metewanne <metewanne.orakaonojeje@sky.uk>
2022-04-27 15:58:12 +0100

    made a car brand list and tests to check if it's empty/if class exists

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
-
-    public static void main(String [] args){
-
-
-
+    public List<String> showBrandList() {
+        List<String> carBrands = new ArrayList<>();
+        carBrands.add("BMW");
+        carBrands.add("Tesla");
+        carBrands.add("Mercedes");
+        carBrands.add("Rolls Royce");
+        carBrands.add("Ferrari");
+        return carBrands;
     }
 
+    public static void main(String[] args){
+
+    }
 
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,30 +1,28 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarTest {
 
+    Car car = new Car();
     //@Test
     //Check that car class exists
     @Test
-    public void testCheckClass(){
-        Car car = new Car();
+    public void testClassExists() {
+        try {
+            Class.forName("Car");
+        } catch (ClassNotFoundException e) {
+            Assert.fail("Should have class called Car");
+        }
     }
 
-    //@Test
-    //Check the car object is not empty
-
-    //@Test
-    //Check the car object has a list of brands
-
-
-
-
-//    @Test
-//    public void testCarClass(){
-//        assertThrows(Exception.class, () -> car.);
-//    }
-
+    @Test
+    public void testEmptyList() throws Exception {
+        assertThat(car.showBrandList().size()).isGreaterThan(0);
+    }
 
 }
