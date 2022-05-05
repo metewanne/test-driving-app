45ac900360e35589260db1e1445ef377365320ac
rosa03 <rsalih03@hotmail.com>
2022-05-05 10:47:47 +0100

    clean up code

--- src/main/java/Brand.java
+++ src/main/java/Brand.java
@@ -3,18 +3,12 @@ import java.util.ArrayList;
 public class Brand {
 
     private String brandName;
-    private ArrayList<CarModel> carModelList;
 
     public Brand() {
     }
 
     public Brand(String brandName){
         this.brandName = brandName;
-    }
-
-    public Brand(String brandName, ArrayList<CarModel> carModelList) {
-        this.brandName = brandName;
-        this.carModelList = carModelList;
     }
 
     //Brand variable
@@ -26,22 +20,4 @@ public class Brand {
         this.brandName = brandName;
     }
 
-    public ArrayList<CarModel> getCarModelList() {
-        return carModelList;
-    }
-
-    public void setCarModelList(ArrayList<CarModel> carModelList) {
-        this.carModelList = carModelList;
-    }
-
-    public String toString(){
-       return getBrandName() + " " + getCarModelList();
-    }
-
-    //     void printCarModels() {
-//
-//     for (CarModel c : this.carModelList) {
-//          System.out.println(c);
-//      }
-//     }
 }
